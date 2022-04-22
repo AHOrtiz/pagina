c012f531db51b8161b1a5e9a0215f1d57132ed47
unknown <RYZEN7@SISTEMAS-WEB.aguainmaculada.local>
2022-04-22 10:53:01 -0500

    Pagina terminada

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
 const hambueguer= document.querySelector('.hamburguer');
 const menu=document.querySelector('.menu-navegacion');
 
-console.log(menu);
-console.log(hambueguer);
+// console.log(menu);
+// console.log(hambueguer);
 
 hambueguer.addEventListener('click',()=>{
     menu.classList.toggle("spread")
 })
 window.addEventListener('click', e=>{
-    
+    if(menu.classList.contains('spread') && e.target !=menu && e.target !=hambueguer){
+        menu.classList.toggle("spread")
+    }
 })
